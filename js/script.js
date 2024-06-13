95bb96c766651874f3bf69ebef81a2ee521d7980
BeneArp <benedetta.arpino@gmail.com>
2024-06-13 16:25:19 +0200

    feat: add play button layout

--- js/script.js
+++ js/script.js
@@ -4,4 +4,6 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-console.log("ciao");
+
+
+// RIFERIMENTI DAL DOM
